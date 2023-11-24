db507c6d9eb6f9799195f7d4916829978d66dcbc
Kwonjeongin <ad160515@gmail.com>
2023-11-24 14:41:06 +0900

    UI 에셋 추가 및 임시저장
    * Sky 에셋 추가
    * UI 에셋 추가
    * Start 씬 구현중

---
+++ Assets/99. Assets/1. KJI/DarkLandsUI/Scripts/Equipment/Item.cs
@@ -0,0 +1,16 @@
+using System;
+using UnityEngine;
+
+namespace DarkLandsUI.Scripts.Equipment
+{
+    [Serializable]
+    public class Item
+    {
+        public ItemType itemType;
+        public SlotType slotType;
+        public Sprite sprite;
+        [Min(1)]
+        public int stackedCount = 1;
+        internal Sprite itemImage;
+    }
+}
