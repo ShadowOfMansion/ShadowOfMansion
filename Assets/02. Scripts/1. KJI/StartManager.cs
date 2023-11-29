ab8ead8abe6c1ffbf8d97bc8f807a55b1072bf1e
Kwonjeongin <ad160515@gmail.com>
2023-11-30 00:04:00 +0900

    로딩씬 수정
    *스타트 -> 로딩-> 플레이 씬 연결 완료

---
+++ Assets/02. Scripts/1. KJI/StartManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartManager : MonoBehaviour
+{
+    public string SceneToLoad;
+    
+    public void LoadGame()
+    {
+        SceneManager.LoadScene(SceneToLoad);
+    }
+    }
